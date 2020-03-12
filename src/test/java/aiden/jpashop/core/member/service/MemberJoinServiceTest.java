af0a0946c22b656f60f98d0fc2f3d5ab50c1a6d7
aiden <shin-ilhyun@hanmail.net>
2020-03-12 21:43:15 +0900

    회원 중복가입 service test 추가

--- src/test/java/aiden/jpashop/core/member/service/MemberJoinServiceTest.java
+++ src/test/java/aiden/jpashop/core/member/service/MemberJoinServiceTest.java
@@ -2,6 +2,7 @@ package aiden.jpashop.core.member.service;
 
 import aiden.jpashop.core.member.domain.Member;
 import aiden.jpashop.core.member.domain.MemberRepository;
+import aiden.jpashop.core.member.exception.MemberJoinException;
 import aiden.jpashop.core.support.Address;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +53,33 @@ class MemberJoinServiceTest {
         Member saveMember = memberRepository.findByUsername(username).get();
         assertEquals(result, saveMember.getId());
 
+    }
+
+    @Test
+    public void 회원가입_중복_username_실패() {
+
+        String username = "aidenshin@hanmail.net";
+
+        memberRepository.save(Member.builder()
+                .username(username)
+                .password("1234")
+                .tel("01023423332")
+                .address(new Address("seoul", "sdlfkj", "39212"))
+                .build());
+
+        em.flush();
+        em.clear();
+
+        Member member = Member.builder()
+                .username(username)
+                .password(passwordEncoder.encode("1234"))
+                .tel("01012344556")
+                .address(new Address("seoul", "ddddddd", "07283"))
+                .build();
+
+        //When
+        assertThrows(MemberJoinException.class, () -> memberJoinService.join(member),
+                "중복 가입 신청 시 예외가 발생해야 함");
 
     }
 
